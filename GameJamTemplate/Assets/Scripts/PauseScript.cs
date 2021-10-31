172311033e166e30c060e23aab3666049229fe5e
Elin Masar <elin.masar@gmail.com>
2021-10-31 23:00:25 +0200

    31.10.2021/ fixing issues

--- GameJamTemplate/Assets/Scripts/PauseScript.cs
+++ GameJamTemplate/Assets/Scripts/PauseScript.cs
@@ -9,7 +9,7 @@ public class PauseScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
